61b2c109465e7c9a086e56ff88b9cea8ae9d698b
Roberto Bonvallet <rbonvall@gmail.com>
2009-11-17 18:39:27 -0300

    missing zero

--- vortex-methods/vm.py
+++ vortex-methods/vm.py
@@ -12,7 +12,7 @@ def eval_velocity(x, y, circ):
     u, v = zeros_like(x), zeros_like(y)
     for p, (x_p, y_p) in enumerate(zip(x, y)):
         K1, K2 = biot_savart_kernel(x - x_p, y - y_p)
-        K1[p], K2[p] = 0.0
+        K1[p], K2[p] = 0.0, 0.0
         u[p], v[p] = dot(circ, K1), dot(circ, K2)
     return u, v
 
