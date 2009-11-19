8de94e01de89d975c7ad4a10a31a7ef3b9f7e22e
Roberto Bonvallet <rbonvall@gmail.com>
2009-11-19 16:11:56 -0300

    LambOseen velocity with quiver

--- vortex-methods/lamb_oseen.py
+++ vortex-methods/lamb_oseen.py
@@ -9,14 +9,22 @@ def lamb_oseen(x, y, t, gamma0=1e0, nu=5e-4):
     den = (4 * nu * t)
     return (gamma0/(den * pi)) * exp(-r_sq/den)
 
+def lamb_oseen_velocity(x, y, t, gamma0=1e0, nu=5e-4):
+    r_sq = x**2 + y**2
+    r = sqrt(x**2 + y**2)
+    c = gamma0/(2 * pi * r)
+    kf = c * (1 - exp(-r_sq/(4 * nu * t)))
+    return -y * kf, x * kf
+
 
 def plot_lamb_oseen():
     N = 2**8
     h = 1/N
     dom = linspace(-0.1, 0.1, N + 1)
     x, y = meshgrid(dom, dom)
+    x_vel, y_vel = x[::40, ::40], y[::40, ::40]
 
-    plot_rows, plot_cols = 2, 4
+    plot_rows, plot_cols = 2, 5
 
     #w0 = h**2 * lamb_oseen(x, y, 0.01)  # VM:T&P, page 27
 
@@ -25,7 +33,7 @@ def plot_lamb_oseen():
 
     t = t0
     iteration = 0
-    for iteration in range(4):
+    for iteration in range(plot_cols - 1):
 
         pylab.subplot(plot_rows, plot_cols, iteration + 1)
         lamb_oseen_values = lamb_oseen(x, y, t)
@@ -37,10 +45,15 @@ def plot_lamb_oseen():
         pylab.colorbar()
 
         pylab.subplot(plot_rows, plot_cols, iteration + 1 + plot_cols)
-        pylab.plot(dom, lamb_oseen(dom, 0, t))
+        u, v = lamb_oseen_velocity(x_vel, y_vel, t)
+        pylab.quiver(x_vel, y_vel, u, v)
 
         iteration += 1
         t += dt
+
+        pylab.subplot(plot_rows, plot_cols, plot_rows * plot_cols)
+        pylab.plot(dom, lamb_oseen(dom, 0, t))
+        pylab.title('Vorticity decay')
 
     pylab.show()
 
