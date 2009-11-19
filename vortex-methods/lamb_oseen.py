78ba29f60d03150ab4859dc5d5ce188c18e6b78a
Roberto Bonvallet <rbonvall@gmail.com>
2009-11-19 15:33:25 -0300

    Lamb Oseen plot refactored

--- vortex-methods/lamb_oseen.py
+++ vortex-methods/lamb_oseen.py
@@ -4,7 +4,7 @@ from __future__ import division
 from numpy import *
 import pylab
 
-def lamb_oseen(x, y, t, gamma0=1e0, nu=1e3):
+def lamb_oseen(x, y, t, gamma0=1e0, nu=5e-4):
     r_sq = x**2 + y**2
     den = (4 * nu * t)
     return (gamma0/(den * pi)) * exp(-r_sq/den)
@@ -13,22 +13,35 @@ def lamb_oseen(x, y, t, gamma0=1e0, nu=1e3):
 def plot_lamb_oseen():
     N = 2**8
     h = 1/N
-    dom = linspace(-1, 1, N + 1)
+    dom = linspace(-0.1, 0.1, N + 1)
     x, y = meshgrid(dom, dom)
-    #
-    w0 = h**2 * lamb_oseen(x, y, 0.01)  # VM:T&P, page 27
-    #
-    for i in range(6):
-        pylab.subplot(2, 6, i + 1)
-        t = .01 + (i + 1) * .0001
-        if i == 0:
-            cs = pylab.contour(x, y, lamb_oseen(x, y, t))
+
+    plot_rows, plot_cols = 2, 4
+
+    #w0 = h**2 * lamb_oseen(x, y, 0.01)  # VM:T&P, page 27
+
+    t0 = 0.1
+    dt = 0.1
+
+    t = t0
+    iteration = 0
+    for iteration in range(4):
+
+        pylab.subplot(plot_rows, plot_cols, iteration + 1)
+        lamb_oseen_values = lamb_oseen(x, y, t)
+        if iteration == 0:
+            cs = pylab.contour(x, y, lamb_oseen_values)
             l = cs.levels
         else:
-            cs = pylab.contour(x, y, lamb_oseen(x, y, t), l)
+            cs = pylab.contour(x, y, lamb_oseen_values, l)
         pylab.colorbar()
-        pylab.subplot(2, 6, i + 7)
+
+        pylab.subplot(plot_rows, plot_cols, iteration + 1 + plot_cols)
         pylab.plot(dom, lamb_oseen(dom, 0, t))
+
+        iteration += 1
+        t += dt
+
     pylab.show()
 
 def main():
