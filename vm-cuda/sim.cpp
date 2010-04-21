4404c8f726c201fb7958557df4f44467209273b6
Roberto Bonvallet <rbonvall@gmail.com>
2010-04-21 11:28:46 -0400

    Simple VM test simulation

---
+++ vm-cuda/sim.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "vm.hpp"
+#include <boost/foreach.hpp>
+
+int main(int argc, char *argv[]) {
+    std::vector<particle> particles;
+    read_particles(particles);
+
+    VortexMethod vm(particles, 3e-2);
+    vm.evaluate_velocity();
+
+    BOOST_FOREACH(particle& p, vm.particles) {
+        std::cout << p.x << ' ' << p.y << ' ' << p.circ << ' ' <<
+                     p.u << ' ' << p.v << std::endl;
+    }
+
+
+}
+
