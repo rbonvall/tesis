63171eb209c178d7ffa84ca2fa500b16f7a174b2
Roberto Bonvallet <rbonvall@gmail.com>
2010-04-25 22:08:43 -0400

    Read viscosity from CL in sim.cpp

--- vm-cuda/sim.cpp
+++ vm-cuda/sim.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 #include "vm.hpp"
+#include "options.hpp"
 #include <boost/foreach.hpp>
 
 int main(int argc, char *argv[]) {
+    lamb_oseen_options ops(argc, argv);
+
     std::vector<particle> particles;
     read_particles(particles);
 
-    VortexMethod vm(particles, 3e-2);
+    VortexMethod vm(particles, ops.nu);
     vm.evaluate_velocity();
 
     std::cout << "# Particles after velocity evaluation" << std::endl;
