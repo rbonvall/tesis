fb8370709c0cdb821dfbe92be294c20db6babe4d
Roberto Bonvallet <rbonvall@gmail.com>
2010-04-25 21:51:41 -0400

    Move option parsing to options.hpp

--- vm-cuda/init.cpp
+++ vm-cuda/init.cpp
@@ -1,37 +1,21 @@
 #include "vm.hpp"
 #include "lamboseen.hpp"
+#include "options.hpp"
 #include <iostream>
 #include <iterator>
 #include <vector>
 #include <algorithm>
 #include <boost/foreach.hpp>
 
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
 #define EXIT_IF(cond, error_msg) \
         do if(cond) {std::cerr << error_msg << std::endl; return 1;} while (0);
 
 int main(int argc, char *argv[]) {
-    float gamma0, nu, x0, x1, y0, y1, h, t0, circulation_threshold;
+    lamb_oseen_options ops(argc, argv);
 
-    po::options_description desc;
-#   define OPTION(op, var, defval) ((op), po::value<float>(&(var))->default_value(defval))
-    desc.add_options()
-        OPTION("total-circulation", gamma0, 1.0)
-        OPTION("circ-threshold", circulation_threshold, 1e-5)
-        OPTION("viscosity",   nu, 5e-4)
-        OPTION("cell-size,h", h,  7.8125e-3)
-        OPTION("x0", x0, -0.3)
-        OPTION("x1", x1, +0.3)
-        OPTION("y0", y0, -0.3)
-        OPTION("y1", y1, +0.3)
-        OPTION("t",  t0, 4.00)
-    ;
-#   undef OPTION
-    po::variables_map vars;
-    po::store(po::parse_command_line(argc, argv, desc), vars);
-    po::notify(vars);
+    float x0 = ops.x0, x1 = ops.x1;
+    float y0 = ops.y0, y1 = ops.y1;
+    float h = ops.h;
 
     unsigned nr_cells = static_cast<unsigned>((x1 - x0) / h) *
                         static_cast<unsigned>((y1 - y0) / h);
@@ -39,15 +23,15 @@ int main(int argc, char *argv[]) {
     EXIT_IF(x0 >= x1 || y0 >= y1, "Degenerate geometry.");
     EXIT_IF(nr_cells == 0, "No room for particles.");
 
-    lamb_oseen_vortex v(gamma0, nu);
+    lamb_oseen_vortex v(ops.gamma0, ops.nu);
     std::vector<particle> particles;
     particles.reserve(nr_cells);
 
     for (float x = x0 + h/2; x <= x1 - h/2; x += h)
         for (float y = y0 + h/2; y <= y1 - h/2; y += h) {
-            float vort = v(x, y, t0);
+            float vort = v(x, y, ops.t0);
             float circ = vort * h * h;
-            if (circ > circulation_threshold) {
+            if (circ > ops.circulation_threshold) {
                 particle p(x, y, vort * h * h);
                 particles.push_back(p);
             }
