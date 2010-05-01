8d8dccf30b15f24f7d2c1200a915d44c834c66e0
Roberto Bonvallet <rbonvall@gmail.com>
2010-04-30 23:13:00 -0400

    Temp files, header message

--- vm-cuda/run-tests.sh
+++ vm-cuda/run-tests.sh
@@ -2,10 +2,16 @@
 
 PATH=.:$PATH
 
-# Measure error of velocity evaluation
-# against analytical Lamb-Oseen velocity
-init | rm-comments.sh > PARTICLES
-vel-eval < PARTICLES > EVALUATED_VELOCITY
-analytical-vel < PARTICLES > ANALYTICAL_VELOCITY
-velocity-error.py EVALUATED_VELOCITY ANALYTICAL_VELOCITY
+PARTICLES=$(mktemp)
+EVALUATED_VELOCITY=$(mktemp)
+ANALYTICAL_VELOCITY=$(mktemp)
+
+cat <<EOF
+### Measure error of velocity evaluation
+### against analytical Lamb-Oseen velocity
+EOF
+init | rm-comments.sh > $PARTICLES
+vel-eval < $PARTICLES > $EVALUATED_VELOCITY
+analytical-vel < $PARTICLES > $ANALYTICAL_VELOCITY
+velocity-error.py $EVALUATED_VELOCITY $ANALYTICAL_VELOCITY
 
