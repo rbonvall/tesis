a8a41a2a160a73d6b6060b893d9a389a1c066c57
Roberto Bonvallet <rbonvall@gmail.com>
2010-05-03 18:51:34 -0400

    Output integration results

--- vm-cuda/integrate.cpp
+++ vm-cuda/integrate.cpp
@@ -15,13 +15,21 @@ int main(int argc, char *argv[]) {
 
     float t = ops.t0, time_step = 0.01;
     for (unsigned iteration = 0; iteration < 100; ++iteration) {
-        std::cout << "# iteration " << iteration << std::endl;
+        std::cout << "##########################" << std::endl;
+        std::cout << "### ITERATION " << iteration << std::endl;
+        std::cout << "# t =  " << t << std::endl;
+
         double start = omp_get_wtime();
         vm.evaluate_velocity();
         vm.convect(time_step);
-        t += time_step;
         double time = omp_get_wtime() - start;
-        std::cout << "#   time measured: " << time << std::endl;
+
+        std::cout << "# iteration timing: " << time << std::endl;
+        std::copy(particles.begin(), particles.end(),
+                  std::ostream_iterator<particle>(std::cout, "\n"));
+        std::cout << std::endl;
+
+        t += time_step;
     }
 
 }
